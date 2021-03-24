5771f500ff16c57de077224ebee09e48b4481ef3
jakhongir001 <jakhongirabdukhamidov@gmail.com>
2021-03-24 19:15:31 +0500

    displaying movements function added

--- script.js
+++ script.js
@@ -61,6 +61,23 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = '';
+  movements.forEach(function (currentMov, i) {
+    const type = currentMov > 0 ? 'deposit' : 'withdrawal';
+    const html = `
+            <div class="movements__row">
+              <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+              <div class="movements__value">${currentMov}</div>
+            </div>
+    `;
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+displayMovements(account1.movements);
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
