9ffb5c4d325595adfdff5d87f46ba735cdb7dcd8
jakhongir001 <jakhongirabdukhamidov@gmail.com>
2021-03-28 14:07:58 +0500

    Enable Get Loan Functionality

--- script.js
+++ script.js
@@ -174,6 +174,21 @@ btnTransfer.addEventListener('click', function (e) {
   }
 });
 
+// Loan fuction
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const amount = Number(inputLoanAmount.value);
+
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+    currentAccount.movements.push(amount);
+    updateUI(currentAccount);
+    inputLoanAmount.blur();
+    inputLoanAmount.value = '';
+  }
+});
+
+// close account functionality
 btnClose.addEventListener('click', function (e) {
   e.preventDefault();
 
